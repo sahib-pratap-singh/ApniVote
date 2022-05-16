5c2d683790f534d4d3d3bc63d44a0ee0e6e4e1b8
Sahib pratap singh <sahibpratapsingh@gmail.com>
2022-05-16 20:07:45 +0530

    Design changed

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Made with <i class="fas fa-heart" ></i> by Sahib pratap singh
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
